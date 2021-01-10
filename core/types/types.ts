504fb234908ed28d0f1ae15e1a244eafe2fa697b
Ezzat Chamudi <echamudi@outlook.com>
2021-01-10 17:41:21 +0300

    Remove AnalysisConfig and AnalysisResult

--- core/types/types.ts
+++ core/types/types.ts
@@ -6,33 +6,33 @@ import { ElementCountExtractResult } from "./factors";
 import { ImageElementsExtractResult, TextElementsExtractResult, BrowserInfoExtractResult, FeatureExtractorResultPhase2 } from './feature-extractor';
 import { FinalScore } from "Core/evaluator/score-calculator/final";
 
-export interface AnalysisConfig {
-    textSize: TextSizeConfig,
-    dominantColors: DominantColorsConfig,
-    pictures: PicturesConfig,
-    symmetry: SymmetryConfig,
-    density: DensityConfig
-}
+// export interface AnalysisConfig {
+//     textSize: TextSizeConfig,
+//     dominantColors: DominantColorsConfig,
+//     pictures: PicturesConfig,
+//     symmetry: SymmetryConfig,
+//     density: DensityConfig
+// }
 
-export interface AnalysisResult {
-    html: string,
-    analysisConfig: AnalysisConfig,
+// export interface AnalysisResult {
+//     html: string,
+//     analysisConfig: AnalysisConfig,
 
-    screenshot: string,
-    textSizeResult: TextSizeExtractResult,
-    textFontTypeResult: TextFontTypeExtractResult,
-    picturesResult: PicturesExtractResult,
-    dominantColorsResult: DominantColorsExtractResult,
-    elementCountResult: ElementCountExtractResult,
-    browserInfoResult: BrowserInfoExtractResult,
-    symmetryResult: SymmetryExtractResult,
-    colorCountResult: ColorCountExtractResult,
-    densityResult: DensityExtractResult,
-    negativeSpaceResult: NegativeSpaceExtractResult,
-    videosResult: VideosExtractResult,
-    imageElementsResult: ImageElementsExtractResult,
-    textElementsResult: TextElementsExtractResult
-}
+//     screenshot: string,
+//     textSizeResult: TextSizeExtractResult,
+//     textFontTypeResult: TextFontTypeExtractResult,
+//     picturesResult: PicturesExtractResult,
+//     dominantColorsResult: DominantColorsExtractResult,
+//     elementCountResult: ElementCountExtractResult,
+//     browserInfoResult: BrowserInfoExtractResult,
+//     symmetryResult: SymmetryExtractResult,
+//     colorCountResult: ColorCountExtractResult,
+//     densityResult: DensityExtractResult,
+//     negativeSpaceResult: NegativeSpaceExtractResult,
+//     videosResult: VideosExtractResult,
+//     imageElementsResult: ImageElementsExtractResult,
+//     textElementsResult: TextElementsExtractResult
+// }
 
 export interface AppState {
     analyzingStatus: string,
