ba0035622f12034876f6b982ade163673efb6a86
Ezzat Chamudi <echamudi@outlook.com>
2021-01-10 17:37:59 +0300

    Comment old text size script

--- core/evaluator/content-side/text-size.ts
+++ core/evaluator/content-side/text-size.ts
@@ -1,49 +1,47 @@
-// TEMP
+// // import { styleElementFactory, getStyleElement } from "Core/utils/style-tools";
+// import { TextSizeConfig, TextSizeExtractResult } from 'Core/types/factors';
 
-// import { styleElementFactory, getStyleElement } from "Core/utils/style-tools";
-import { TextSizeConfig, TextSizeExtractResult } from 'Core/types/factors';
+// /**
+//  * @param doc elements to be evaluated, ideally all elements in the page
+//  */
+// export function textSize(doc: Document): TextSizeExtractResult {
+//     const elements: NodeListOf<Element> = doc.querySelectorAll('body *');
 
-/**
- * @param doc elements to be evaluated, ideally all elements in the page
- */
-export function textSize(doc: Document): TextSizeExtractResult {
-    const elements: NodeListOf<Element> = doc.querySelectorAll('body *');
+//     const all = elements;
 
-    const all = elements;
+//     const textSizeMap: Record<number, number> = {};
 
-    const textSizeMap: Record<number, number> = {};
+//     // Mark letters with too small letters
+//     for (let i = 0, max = all.length; i < max; i += 1) {
+//         const currentEl = all[i] as HTMLElement;
 
-    // Mark letters with too small letters
-    for (let i = 0, max = all.length; i < max; i += 1) {
-        const currentEl = all[i] as HTMLElement;
+//         let text = '';
+//         currentEl.childNodes.forEach((cn) => {
+//             if (cn.nodeType === Node.TEXT_NODE) text += cn.textContent ?? '';
+//         })
+//         text = text.trim();
 
-        let text = '';
-        currentEl.childNodes.forEach((cn) => {
-            if (cn.nodeType === Node.TEXT_NODE) text += cn.textContent ?? '';
-        })
-        text = text.trim();
+//         const bound = currentEl.getBoundingClientRect();
+//         const invisible = bound.width === 0 && bound.height === 0;
 
-        const bound = currentEl.getBoundingClientRect();
-        const invisible = bound.width === 0 && bound.height === 0;
+//         if (text !== '' && !invisible) {
+//             const fontSize = parseInt(getComputedStyle(currentEl).fontSize, 10);
 
-        if (text !== '' && !invisible) {
-            const fontSize = parseInt(getComputedStyle(currentEl).fontSize, 10);
+//             if (textSizeMap[fontSize] === undefined) {
+//                 textSizeMap[fontSize] = [...text].length;
+//             } else {
+//                 textSizeMap[fontSize] += [...text].length;
+//             }
+//         }
+//     }
 
-            if (textSizeMap[fontSize] === undefined) {
-                textSizeMap[fontSize] = [...text].length;
-            } else {
-                textSizeMap[fontSize] += [...text].length;
-            }
-        }
-    }
+//     let totalCharacters = 0;
+//     Object.keys(textSizeMap).forEach((key) => {
+//         totalCharacters += textSizeMap[key as unknown as number];
+//     })
 
-    let totalCharacters = 0;
-    Object.keys(textSizeMap).forEach((key) => {
-        totalCharacters += textSizeMap[key as unknown as number];
-    })
-
-    return { totalCharacters, textSizeMap };
-}
+//     return { totalCharacters, textSizeMap };
+// }
 
 // export function textSizeStyler(config: TextSizeConfig) {
 //     styleElementFactory('textSize');
