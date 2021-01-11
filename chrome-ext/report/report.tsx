51b89f3a3da6766f2ddfcd7de2f566537304f01e
Ezzat Chamudi <echamudi@outlook.com>
2021-01-11 15:30:35 +0300

    Initial overview page

--- chrome-ext/report/report.tsx
+++ chrome-ext/report/report.tsx
@@ -4,220 +4,253 @@ import { render } from "react-dom";
 import { DefaultButton, PrimaryButton, Stack, IStackTokens, initializeIcons } from 'office-ui-fabric-react';
 import { Nav, INavStyles, INavLinkGroup } from 'office-ui-fabric-react/lib/Nav';
 import { ScrollablePane, ScrollbarVisibility } from 'office-ui-fabric-react/lib/ScrollablePane';
+import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
+import * as moment from 'moment';
 
 initializeIcons();
 
 const navStyles: Partial<INavStyles> = { root: { width: 300 } };
 
 const navLinkGroups: INavLinkGroup[] = [
-    {
+  {
+    name: 'Overview',
+    links: [
+      {
         name: 'Overview',
-        links: [
-            {
-                name: 'Overview',
-                key: 'meta-overview',
-                url: ''
-            },
-            {
-                name: 'Element Count',
-                key: 'meta-element-count',
-                url: ''
-            },
-            {
-                name: 'Detection Visualization',
-                key: 'meta-detection-visualization',
-                url: ''
-            },
-        ],
-    },
-    {
-        name: 'Distribution of Components',
-        links: [
-            {
-                name: 'Symmetry (Pixel LR)',
-                key: 'symmetry-pixel-l-r',
-                url: ''
-            },
-            {
-                name: 'Symmetry (Pixel TB)',
-                key: 'symmetry-pixel-t-b',
-                url: ''
-            },
-            {
-                name: 'Complexity (Text DOM)',
-                key: 'complexity-text-dom',
-                url: ''
-            },
-            {
-                name: 'Density (Pixel)',
-                key: 'density-pixel',
-                url: ''
-            },
-            {
-                name: 'Density (DOM)',
-                key: 'density-major-dom',
-                url: ''
-            },
-        ],
-    },
-    {
-        name: 'Appearance of Components',
-        links: [
-            {
-                name: 'Cohesion (Image DOM)',
-                key: 'cohesion-image-dom',
-                url: ''
-            },
-            {
-                name: 'Economy (Image DOM)',
-                key: 'economy-image-dom',
-                url: ''
-            },
-            {
-                name: 'Economy (Text DOM)',
-                key: 'economy-text-dom',
-                url: ''
-            },
-        ],
-    },
-    {
-        name: 'Alignment of Components',
-        links: [
-            {
-                name: 'Simplicity (Horizontal)',
-                key: 'simplicity-horizontal',
-                url: ''
-            },
-            {
-                name: 'Simplicity (Vertical)',
-                key: 'simplicity-vertical',
-                url: ''
-            },
-        ],
-    },
-    {
-        name: 'Color Usage',
-        links: [
-            {
-                name: 'Dominant Colors',
-                key: 'dominant-colors',
-                url: ''
-            },
-        ],
-    },
-    {
-        name: 'Graphics',
-        links: [
-            {
-                name: 'Use of Pictures',
-                key: 'graphic-pictures',
-                url: ''
-            },
-            {
-                name: 'Use of Videos',
-                key: 'graphic-videos',
-                url: ''
-            },
-        ],
-    },
-    {
-        name: 'Typography',
-        links: [
-            {
-                name: 'Text Size',
-                key: 'text-size',
-                url: ''
-            },
-            {
-                name: 'Type of Font',
-                key: 'text-total-fonts',
-                url: ''
-            },
-            {
-                name: 'Type of Font',
-                key: 'text-font-type',
-                url: ''
-            },
-        ],
-    },
-    {
-        name: 'Structure',
-        links: [
-            {
-                name: 'Negative Space (Text)',
-                key: 'negative-space-text',
-                url: ''
-            },
-        ],
-    },
+        key: 'meta-overview',
+        url: ''
+      },
+      {
+        name: 'Element Count',
+        key: 'meta-element-count',
+        url: ''
+      },
+      {
+        name: 'Detection Visualization',
+        key: 'meta-detection-visualization',
+        url: ''
+      },
+    ],
+  },
+  {
+    name: 'Distribution of Components',
+    links: [
+      {
+        name: 'Symmetry (Pixel LR)',
+        key: 'symmetry-pixel-l-r',
+        url: ''
+      },
+      {
+        name: 'Symmetry (Pixel TB)',
+        key: 'symmetry-pixel-t-b',
+        url: ''
+      },
+      {
+        name: 'Complexity (Text DOM)',
+        key: 'complexity-text-dom',
+        url: ''
+      },
+      {
+        name: 'Density (Pixel)',
+        key: 'density-pixel',
+        url: ''
+      },
+      {
+        name: 'Density (DOM)',
+        key: 'density-major-dom',
+        url: ''
+      },
+    ],
+  },
+  {
+    name: 'Appearance of Components',
+    links: [
+      {
+        name: 'Cohesion (Image DOM)',
+        key: 'cohesion-image-dom',
+        url: ''
+      },
+      {
+        name: 'Economy (Image DOM)',
+        key: 'economy-image-dom',
+        url: ''
+      },
+      {
+        name: 'Economy (Text DOM)',
+        key: 'economy-text-dom',
+        url: ''
+      },
+    ],
+  },
+  {
+    name: 'Alignment of Components',
+    links: [
+      {
+        name: 'Simplicity (Horizontal)',
+        key: 'simplicity-horizontal',
+        url: ''
+      },
+      {
+        name: 'Simplicity (Vertical)',
+        key: 'simplicity-vertical',
+        url: ''
+      },
+    ],
+  },
+  {
+    name: 'Color Usage',
+    links: [
+      {
+        name: 'Dominant Colors',
+        key: 'dominant-colors',
+        url: ''
+      },
+    ],
+  },
+  {
+    name: 'Graphics',
+    links: [
+      {
+        name: 'Use of Pictures',
+        key: 'graphic-pictures',
+        url: ''
+      },
+      {
+        name: 'Use of Videos',
+        key: 'graphic-videos',
+        url: ''
+      },
+    ],
+  },
+  {
+    name: 'Typography',
+    links: [
+      {
+        name: 'Text Size',
+        key: 'text-size',
+        url: ''
+      },
+      {
+        name: 'Type of Font',
+        key: 'text-total-fonts',
+        url: ''
+      },
+      {
+        name: 'Type of Font',
+        key: 'text-font-type',
+        url: ''
+      },
+    ],
+  },
+  {
+    name: 'Structure',
+    links: [
+      {
+        name: 'Negative Space (Text)',
+        key: 'negative-space-text',
+        url: ''
+      },
+    ],
+  },
 ];
 
 class App extends React.Component {
-    public state = {
-        currentPage: 'overview',
-        webPageData: null,
-    };
+  public state: {
+    currentPage: string,
+    webPageData: WebPageData | null,
+    reportData__timestamp?: string
+  } = {
+    currentPage: 'overview',
+    webPageData: null
+  };
 
-    constructor(props: any) {
-        super(props);
+  constructor(props: any) {
+    super(props);
 
-        navLinkGroups.forEach(group=> {
-            group.links.forEach(link => {
-                link.forceAnchor = true;
-                link.onClick = () => {
-                    this.setState((prevStates) => ({
-                        ...prevStates,
-                        currentPage: link.key ?? ''
-                    }));
+    navLinkGroups.forEach(group=> {
+      group.links.forEach(link => {
+        link.forceAnchor = true;
+        link.onClick = () => {
+          this.setState((prevStates) => ({
+            ...prevStates,
+            currentPage: link.key ?? ''
+          }));
+        }
+      })
+    });
+
+    chrome.storage.local.get(['webPageData'], (items) => {
+      this.setState((prevStates) => ({
+        ...prevStates,
+        webPageData: items['webPageData']
+      }));
+
+      this.prepareReport();
+    });
+  }
+
+  prepareReport() {
+    if (this.state.webPageData === null) return;
+
+    const timestamp = moment.unix(this.state.webPageData.timestamp / 1000).format('LLLL');
+
+    this.setState((prevStates) => ({
+      ...prevStates,
+      reportData__timestamp: timestamp
+    }));
+  }
+
+  render() {
+    if (this.state.webPageData === null) {
+      return (
+        <div></div>
+      )
+    }
+
+    return (
+      <div className="report">
+        <div className="report-navbar">
+          <div className="report-navbar-title">
+            Smart Web Design Scraper
+          </div>
+        </div>
+        <div className="report-content">
+          <div className="report-sidebar">
+            <Nav styles={navStyles} groups={navLinkGroups} />
+          </div>
+          <div className="report-details">
+            { this.state.webPageData !== null &&
+              <>
+                {this.state.currentPage === 'meta-overview' &&
+                  <div className="report-details-container">
+                    <h1>
+                      Overview
+                    </h1>
+                    <hr/>
+                    <p>
+                      <b>URL: </b> {this.state.webPageData.featureExtractorResult.browserInfo.url}<br/>
+                      <b>Date: </b> {this.state.reportData__timestamp}<br/>
+                    </p>
+                    <img src={this.state.webPageData.screenshot} style={{width: 800}}/>
+                  </div>
                 }
-            })
-        });
-
-        chrome.storage.local.get(['webPageData'], (items) => {
-            this.setState((prevStates) => ({
-                ...prevStates,
-                webPageData: items['webPageData']
-            }));
-
-            console.log(this.state.webPageData);
-        });
-    }
-
-    render() {
-        return (
-            <div className="report">
-                <div className="report-navbar">
-                    <div className="report-navbar-title">
-                        Smart Web Design Scraper
-                    </div>
-                </div>
-                <div className="report-content">
-                    <div className="report-sidebar">
-                        <Nav styles={navStyles} groups={navLinkGroups} />
-                    </div>
-                    <div className="report-details">
-                        {this.state.currentPage === 'meta-overview' &&
-                            <div className="report-details-container">
-                                <h1>
-                                    Overview
-                                </h1>
-                            </div>
-                        }
-                        {this.state.currentPage === 'meta-element-count' &&
-                            <div>
-                                Element Count
-                            </div>
-                        }
-                        {this.state.currentPage === 'meta-detection-visualization' &&
-                            <div>
-                                Detection Visualization
-                            </div>
-                        }
-                    </div>
-                </div>
-            </div>
-        )
-    }
+                {this.state.currentPage === 'meta-element-count' &&
+                  <div>
+                    Element Count
+                  </div>
+                }
+                {this.state.currentPage === 'meta-detection-visualization' &&
+                  <div>
+                    Detection Visualization
+                  </div>
+                }
+              </>
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 const rootElement = document.getElementById("root");
