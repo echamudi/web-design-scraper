f3ca4de6efa84751251b21254fb302f66788d277
Ezzat Chamudi <echamudi@outlook.com>
2021-01-16 18:03:54 +0300

    Add imageToBufferArray and error handling

--- core/utils/image-canvas.ts
+++ core/utils/image-canvas.ts
@@ -16,6 +16,20 @@ export async function imageToCanvas(imageURI: string): Promise<HTMLCanvasElement
 
             resolve(canvas);
         };
+        img.onerror = function() {
+            reject(new Error('Error on loading the imageURI'));
+        }
         img.src = imageURI;
     });
 };
+
+export async function imageToBufferArray(imageURI: string): Promise<Uint8ClampedArray> {
+    const canvas = await imageToCanvas(imageURI);
+    const ctx = canvas.getContext('2d');
+    if (ctx === null) throw new Error('CTX is null');
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const imagePixels = imageData.data;
+
+    return imagePixels;
+}
