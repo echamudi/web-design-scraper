bcf6b8cf70c5663e4c8d210a913f8edbb24241f1
Ezzat Chamudi <echamudi@outlook.com>
2021-01-18 20:21:31 +0300

    Add ImageData from canvas lib

--- jest-environment-custom.js
+++ jest-environment-custom.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-const { Image } = require('canvas');
+const { Image, ImageData } = require('canvas');
 
 const JSDOMEnvironment = require('jest-environment-jsdom');
 
@@ -14,11 +14,14 @@ class CustomEnvironment extends JSDOMEnvironment {
   async setup() {
     await super.setup();
     this.temp.Image = this.global.Image;
+    this.temp.ImageData = this.global.ImageData;
     this.global.Image = Image;
+    this.global.ImageData = ImageData;
   }
 
   async teardown() {
     this.global.Image = this.temp.Image;
+    this.global.ImageData = this.temp.ImageData;
     await super.teardown();
   }
   /**
