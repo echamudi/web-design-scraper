ea3d0845b3fddc77a86b663a1e35f5e43a991217
Ezzat Chamudi <echamudi@outlook.com>
2021-01-18 20:21:10 +0300

    Add image data pixel accessor

--- core/utils/image-canvas.test.ts
+++ core/utils/image-canvas.test.ts
@@ -1,6 +1,6 @@
 import imageToBase64 from 'image-to-base64';
 import path from 'path';
-import { imageToCanvas, imageToBufferArray } from 'Core/utils/image-canvas';
+import { getPixelImageData, imageToCanvas, imageToImageData, setPixelImageData } from 'Core/utils/image-canvas';
 
 test('imageToCanvas', async () => {
     try {
@@ -10,9 +10,9 @@ test('imageToCanvas', async () => {
     }
 });
 
-test('imageToBufferArray', async () => {
+test('imageToImageData', async () => {
     const img = await imageToBase64(path.join(__dirname, '../../test/fixtures/small-imgs/rgba.png'));
-    const bufferResult = await imageToBufferArray('data:image/png;base64,' + img);
+    const bufferResult = (await imageToImageData('data:image/png;base64,' + img)).data;
     const bufferSpec: Uint8ClampedArray = new Uint8ClampedArray([
         0,   0, 255, 255,   // Blue
         0, 255,   0, 255,   // Green
@@ -31,4 +31,29 @@ test('imageToBufferArray', async () => {
     ]);
 
     expect([...bufferResult]).toEqual([...bufferSpec]);
+});
+
+test('getPixelImageData', async () => {
+    const img = await imageToBase64(path.join(__dirname, '../../test/fixtures/small-imgs/four-colors.png'));
+    const imageData = await imageToImageData('data:image/png;base64,' + img);
+
+    expect(getPixelImageData(imageData, 0, 0)).toStrictEqual({ r: 255, g: 0, b: 0, a: 255 });
+    expect(getPixelImageData(imageData, 3, 3)).toStrictEqual({ r: 255, g: 0, b: 0, a: 255 });
+    expect(getPixelImageData(imageData, 7, 4)).toStrictEqual({ r: 255, g: 255, b: 0, a: 255 });
+    expect(getPixelImageData(imageData, 9, 9)).toStrictEqual({ r: 255, g: 255, b: 0, a: 255 });
+
+    const img2 = await imageToBase64(path.join(__dirname, '../../test/fixtures/small-imgs/rgba.png'));
+    const imageData2 = await imageToImageData('data:image/png;base64,' + img2);
+
+    expect(getPixelImageData(imageData2, 2, 1)).toStrictEqual({ r: 255, g: 0, b: 0, a: 255 });
+});
+
+test('setPixelImageData', async () => {
+    const imageData = new ImageData(4, 3);
+
+    expect(getPixelImageData(imageData, 2, 1)).toStrictEqual({ r: 0, g: 0, b: 0, a: 0 });
+    setPixelImageData(imageData, 2, 1, {r: 4, g: 5, b: 6, a: 7});
+    expect(getPixelImageData(imageData, 2, 1)).toStrictEqual({r: 4, g: 5, b: 6, a: 7});
+    setPixelImageData(imageData, 2, 1, {r: 11, g: 12, b: 13});
+    expect(getPixelImageData(imageData, 2, 1)).toStrictEqual({r: 11, g: 12, b: 13, a: 255});
 });
--- core/utils/image-canvas.ts
+++ core/utils/image-canvas.ts
@@ -1,3 +1,5 @@
+import { Color } from "Core/types/types";
+
 export async function imageToCanvas(imageURI: string): Promise<HTMLCanvasElement> {
     return new Promise((resolve, reject) => {
         const canvas = document.createElement('canvas');
@@ -23,13 +25,49 @@ export async function imageToCanvas(imageURI: string): Promise<HTMLCanvasElement
     });
 };
 
-export async function imageToBufferArray(imageURI: string): Promise<Uint8ClampedArray> {
+export async function imageToImageData(imageURI: string): Promise<ImageData> {
     const canvas = await imageToCanvas(imageURI);
     const ctx = canvas.getContext('2d');
     if (ctx === null) throw new Error('CTX is null');
 
     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const imagePixels = imageData.data;
+    return imageData;
+}
 
-    return imagePixels;
+/**
+ * Get a pixel in ImageData data
+ * @param x x coordinate, counted from the left
+ * @param y y coordinate, counted from the top
+ */
+export function getPixelImageData(imageData: ImageData, x: number, y: number): Color {
+    const { data, width } = imageData;
+    const index = y * width + x;
+    // if (index > width * height || index < 0)
+    //     throw new Error('imageDataAccessor out of bound');
+    const bufferIndex = index * 4;
+
+    return {
+        r: data[bufferIndex],
+        g: data[bufferIndex + 1],
+        b: data[bufferIndex + 2],
+        a: data[bufferIndex + 3]
+    }
+}
+
+/**
+ * Set a pixel in ImageData data
+ * @param x x coordinate, counted from the left
+ * @param y y coordinate, counted from the top
+ */
+export function setPixelImageData(imageData: ImageData, x: number, y: number, color: Color): boolean {
+    const { data, width } = imageData;
+    const index = y * width + x;
+    const bufferIndex = index * 4;
+
+    data[bufferIndex] = color.r;
+    data[bufferIndex + 1] = color.g;
+    data[bufferIndex + 2] = color.b;
+    data[bufferIndex + 3] = color.a ?? 255;
+
+    return true;
 }
