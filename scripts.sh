a4c0aa92985f1ae736df3820aefb969d757b83fb
Ezzat Chamudi <echamudi@outlook.com>
2021-02-08 15:25:36 +0300

    Update scripts

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,5 @@
 npx eslint ./executor/**/*.ts --fix
 npx eslint ./core/**/*.ts --fix
 npx eslint ./chrome-ext/**/*.{ts,tsx} --fix
+
+license-report --output=html --config license-report-config.json > licenses.html
